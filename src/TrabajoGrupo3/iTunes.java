92a3e22bce3416e6fd9383e2c4b43665668b860b
Gambarelli <Floresgambarelli@gmail.com>
2015-06-12 08:32:18 -0600

    Downloads

--- src/TrabajoGrupo3/iTunes.java
+++ src/TrabajoGrupo3/iTunes.java
@@ -8,6 +8,7 @@ package TrabajoGrupo3;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Calendar;
 
 /**
  *
@@ -39,6 +40,8 @@ public class iTunes {
      */
     
     public static final String ROOT = "iTunesApp";
+    
+    
     
     public void Itunes() throws IOException{
         RandomAccessFile icodigos = new RandomAccessFile(ROOT+ "/codigo.itn", "rw");
@@ -102,9 +105,52 @@ public class iTunes {
         }
     }
     
-    
-    
-    public void songs(String txtFile) throws IOException{
+    public void downloadSong(int codeSong, String cliente) throws IOException{
+        
+        RandomAccessFile isongs = new RandomAccessFile(ROOT+ "/songs.itn", "rw");
+        RandomAccessFile idownloads = new RandomAccessFile(ROOT+ "/codigo.itn", "rw");
+        Calendar c = Calendar.getInstance();
+        int codigo=-1;
+        Double Precio= -1.0;
+        String nombreC="";
+        
+        isongs.seek(0);
+        
+        while(isongs.getFilePointer()<isongs.length()){    
+           if(codeSong==isongs.readInt()){
+            codigo = getCodigo(4);
+            }
+            nombreC = isongs.readUTF();
+            isongs.readUTF();
+            Precio = isongs.readDouble();
+            isongs.readInt();
+            isongs.readInt();
+       
+      }  
+
+        if( codigo != -1 && codigo !=-1.0){
+            
+            idownloads.seek(idownloads.length());
+            long pos = idownloads.getFilePointer();
+            int codigoDi = idownloads.read()+1;
+            idownloads.seek(pos);
+            idownloads.writeInt(codigoDi);
+            idownloads.writeLong(c.getTimeInMillis());
+            idownloads.writeInt(codigo);
+            idownloads.writeUTF(cliente);
+            idownloads.writeDouble(Precio);
+            
+            System.out.println("GRACIAS"+cliente+"Por bajar"+nombreC+"a un costo Lps."+Precio);
+            
+        }
+        
+        
+
+        
         
     }
+    
+    
+    
+    
 }
