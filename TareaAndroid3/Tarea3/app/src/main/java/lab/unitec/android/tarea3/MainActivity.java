5adade11550cee40f10fc78752108ce916bfc776
Gambarelli <Floresgambarelli@gmail.com>
2015-06-25 23:06:32 -0600

    otra lista

--- TareaAndroid3/Tarea3/app/src/main/java/lab/unitec/android/tarea3/MainActivity.java
+++ TareaAndroid3/Tarea3/app/src/main/java/lab/unitec/android/tarea3/MainActivity.java
@@ -8,44 +8,27 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        String[] myList = new String[] {"Hello","World","Foo","Bar"};
-
-        ListView lv = new ListView(this);
-
-        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myList));
-
-        setContentView(lv);
-
+public class MainActivity extends ListActivity {
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
 }
